561ca6d360b4d2cefef7bad1281ac194c1f7ffe4
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-02-24 21:13:03 +0600

    included array map in about me section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import BULLET from "./media/Pointer.svg";
 import photo from "./media/123.jpg";
 
 function App() {
+  const aboutMe = [
+    "SDU, KTL alumni",
+    "Java, HTML, CSS, Javascript, React JS",
+    "English, Russian, Kazakh, Turkish",
+  ];
+
   return (
     <div className="App">
       <header>
@@ -32,22 +38,12 @@ function App() {
             is my portfolio. I’m software developer and this is my portfolio.
           </h5>
           <div className="bullet-points">
-            <div className="bullet-point">
-              <img className="bullet" src={BULLET} alt="bullet point" />
-              <p className="bullet-point-text">Lorem Ipsum Description Text</p>
-            </div>
-            <div className="bullet-point">
-              <img className="bullet" src={BULLET} alt="bullet point" />
-              <p className="bullet-point-text">Lorem Ipsum Description Text</p>
-            </div>
-            <div className="bullet-point">
-              <img className="bullet" src={BULLET} alt="bullet point" />
-              <p className="bullet-point-text">Lorem Ipsum Description Text</p>
-            </div>
-            <div className="bullet-point">
-              <img className="bullet" src={BULLET} alt="bullet point" />
-              <p className="bullet-point-text">Lorem Ipsum Description Text</p>
-            </div>
+            {aboutMe.map((item, index) => (
+              <div className="bullet-point" key={index}>
+                <img className="bullet" src={BULLET} alt="bullet point" />
+                <p className="bullet-point-text">{item}</p>
+              </div>
+            ))}
           </div>
         </div>
         <img className="my-photo" src={photo} alt="photo" />
